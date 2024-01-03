cc207cb11ada2483cf9fe35ca5b944fe515d1a21
OLIVIE RUKABO <133749488+olivierRukaboAsyst@users.noreply.github.com>
2024-01-03 07:51:37 +0100

    docker and spring secutiry

---
+++ src/main/java/lab/anubis/lesavis/LesAvisApplication.java
@@ -0,0 +1,13 @@
+package lab.anubis.lesavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesAvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesAvisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/anubis/lesavis/LesAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.anubis.lesavis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesAvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
